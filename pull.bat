fb05222ff00287ea8f14c483a3ff81ecf7ff5bdd
YUQI\maruiheng <maruiheng@gg.com>
2024-04-24 17:01:11 +0800

    bug 修复

--- pull.bat
+++ pull.bat
@@ -8,19 +8,17 @@ for /d %%i in ("%repo_directory%\*") do (
     cd /d "%%i"
 
     echo 正在更新代码库：%%~nxi
-    git pull -f --rebase)
+    git pull -f --rebase
     REM 检查 git pull 的返回值
-    IF NOT ERRORLEVEL 0 (
+    IF ERRORLEVEL 1 (
       echo 存在local changes，正在删除本地更改：%%~nxi
       git checkout .
-      git clean -d -f
 
-      REM 检查 git clean 的返回值
-      IF NOT ERRORLEVEL 0 (
+      REM 检查的返回值
+      IF ERRORLEVEL 1 (
           echo git clean 失败，删除 .git\index.lock 文件
           del /f .git\index.lock
           git checkout .
-          git clean -d -f
           git pull -f --rebase
       )
     )
