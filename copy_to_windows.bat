f9bb21b1d867bf2206adf7589358231ca53405e7
YUQI\maruiheng <maruiheng@gg.com>
2024-04-24 14:45:10 +0800

    copy to windows 脚本添加

---
+++ copy_to_windows.bat
@@ -0,0 +1,24 @@
+@echo off
+CHCP 65001
+
+cd /d %~dp0
+
+:: 获取管理员权限
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo 请求管理员权限...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    :: 此处添加你的 xcopy 命令
+    xcopy /y "pull.bat" "c:\windows"
+    echo 执行 xcopy 命令完成。
+    pause
